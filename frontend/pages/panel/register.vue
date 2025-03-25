85b8787e0cb5043649909d0b01d8ccacb1de1023
Benoit Chastel <131172923+benTDC@users.noreply.github.com>
2025-03-25 18:14:46 +0100

    ♻️ REFACTOR: Utilisation du composant LanguageSelector.vue dans Register.vue

--- frontend/pages/panel/register.vue
+++ frontend/pages/panel/register.vue
@@ -6,31 +6,8 @@
                <div class="mr-auto">
                     <svgo-logo-hello-humans-full class="w-[220px] fill-[#f7f0e5]"/>
                </div>
-
-               <!-- Sélecteur de langue -->
-               <div ref="langMenuRef" class="inline-flex relative align-top ml-0">
-                    <button
-                         @click="toggleLangMenu"
-                         class="box-border h-[34px] px-[8px] pr-[6px] inline-flex items-center justify-center rounded-[3px] border border-transparent text-[rgb(100,116,145)] text-[14px] leading-[1] uppercase"
-                    >
-                         <svgo-panel-icon-earth class="w-[16px] h-[16px] mr-2 fill-[#647491]" />
-                         {{ selectedLang }}
-                         <svgo-panel-icon-triangle-caret-down class="w-[16px] h-[16px] ml-2 transform transition-transform fill-[#647491]" :class="{ 'rotate-180': isLangMenuOpen }" />
-                    </button>
-
-                    <!-- Liste des langues -->
-                    <div v-if="isLangMenuOpen" class="absolute left-[-51.4562px] min-w-[128px] top-full z-[20] opacity-100 visible translate-y-[4px]">
-                         <div class="max-h-none overflow-y-visible bg-white rounded-[8px] p-[8px] mb-[10px] shadow-[0px_8px_20px_rgba(0,20,51,0.24)]">
-                              <div
-                                   v-for="lang in languages"
-                                   :key="lang.code"
-                                   @click="selectLanguage(lang.code)"
-                                   class="text-[14px] leading-[18px] tracking-[-0.01em] h-[44px] px-[24px] flex items-center text-[rgb(8,15,26)] hover:bg-[rgb(239,242,246)] cursor-pointer">
-                                   {{ lang.name }}
-                              </div>
-                         </div>
-                    </div>
-
+               <div class="login-container">
+                    <LanguageSelector @languageSelected="updateSelectedLang" />
                </div>
           </div>
           <div class="[grid-area:1_/_2_/_3_/_4] relative z-0 overflow-hidden relative before:content-[''] before:absolute before:bottom-0 before:left-1/2 before:-z-10 before:bg-white before:[width:max(3000px,300vh)] before:[height:max(3000px,300vh)] before:[transform:translateX(-50%)]"></div>
@@ -120,6 +97,7 @@
 import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 import PasswordInput from "@/components/panel/PasswordInput.vue";
+import LanguageSelector from "@/components/panel/LanguageSelector.vue";
 
 // Champs du formulaire
 const email = ref('')
@@ -132,27 +110,9 @@ const loginError = ref(false)
 const loading = ref(false)
 
 const router = useRouter()
-// Sélecteur de langue
-const isLangMenuOpen = ref(false)
-const selectedLang = ref('FR')
-const languages = ref([
-     { code: 'DE', name: 'Deutsch' },
-     { code: 'EN', name: 'English' },
-     { code: 'ES', name: 'Español' },
-     { code: 'FR', name: 'Français' },
-     { code: 'IT', name: 'Italiano' },
-     { code: 'PT', name: 'Português' }
-])
 
-const langMenuRef = ref(null) // Référence pour la boîte des langues
-
-const toggleLangMenu = () => {
-     isLangMenuOpen.value = !isLangMenuOpen.value
-}
-
-const selectLanguage = (code) => {
-     selectedLang.value = code
-     isLangMenuOpen.value = false
+const updateSelectedLang = (lang: string) => {
+     console.log('Langue sélectionnée :', lang)
 }
 
 // Fonction pour valider le formulaire
