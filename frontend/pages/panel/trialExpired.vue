5311ea02a67d5fc518fe0d0eef2ba6e66eefe625
Jérémy Vandenostende <jrm@tiensdonc.com>
2025-03-25 06:50:55 +0100

    🛠️ FEATURE: Ajout de la page "Essai terminé"

---
+++ frontend/pages/panel/trialExpired.vue
@@ -0,0 +1,63 @@
+<template>
+     <div id="app-content" class="flex-1 overflow-x-hidden overflow-y-auto relative h-full">
+          <div class="fixed inset-0 z-[100] flex flex-col justify-start items-center bg-[#f5f7f9] overflow-scroll">
+               <div class="bg-white w-full mt-0 mb-0">
+                    <div class="grid place-items-center">
+                         <div class="flex flex-col justify-start items-center w-[560px] text-center pt-10 pb-8">
+                              <span class="block w-6 min-w-[24px] h-6 min-h-[24px] mt-0 mb-0"></span>
+                              <h2 class="mt-0 mb-0 font-medium text-[32px] leading-[41px] tracking-[-0.01em]">Continuez à exploiter pleinement Hellohumans</h2>
+                              <span class="block w-3 min-w-[12px] h-3 min-h-[12px]"></span>
+                              <p class="mt-0 mb-0 font-normal text-[18px] leading-[23px] tracking-[-0.01em] text-[#647491]">Votre essai du <strong>avec toutes les fonctionnalités</strong> a expiré le <strong>24/03/2025</strong>. Continuez sur votre lancée. Comparez les plans et choisissez l'option idéale pour votre entreprise.
+                              </p>
+                              <span class="block w-5 min-w-[20px] h-5 min-h-[20px]"></span>
+                              <a href="/panel/upgrade" class="inline-flex justify-center items-center bg-[#0566ff] border border-[#0566ff] text-white w-auto rounded-[8px] text-[16px] h-[38px] leading-[20px] px-4 py-0 hover:bg-[#0049bd] hover:border-[#0049bd] hover:text-white ">Comparer les offres</a>
+                         </div>
+                    </div>
+               </div>
+               <div class="flex flex-col justify-start items-center w-[764px] text-center pt-14 pb-14">
+                    <div class="flex flex-col justify-start items-start w-[560px]">
+                         <h2 class="w-full mt-0 mb-0 font-medium text-[28px] leading-[33px] tracking-[-0.01em]">Passer à offre Free</h2>
+                         <p class="mt-[8px] mb-0 font-normal text-[16px] leading-[20px] tracking-[-0.01em] text-[#647491]">Vous pouvez à tout moment revenir à une offre inférieure et utiliser notre offre gratuite à vie. Vous pouvez la modifier quand vous le souhaitez.</p>
+                    </div>
+                    <div class="mt-5 flex flex-[1_1_0%] flex-wrap w-full gap-1">
+                         <div class="flex flex-row justify-start items-center w-[calc(50%-2px)] shrink border border-[#e2e8ef] rounded-[4px] p-4 bg-white">
+                              <svgo-panel-icon-chat-bull class="w-[20px] h-[20px] fill-[#647491]"/>
+                              <div class="inline-flex flex-grow max-w-full overflow-hidden ml-[16px]">
+                                   <div class="flex flex-row justify-start items-center max-w-full">
+                                        <div class="w-full">
+                                             <p class="mt-0 mb-0 font-normal text-[14px] leading-[18px] tracking-[-0.01em] text-left overflow-hidden text-ellipsis whitespace-nowrap text-[#080f1a] border-b border-dashed border-[#acb8cb] cursor-default">Conversations traitées - <strong>JUSQU’À 50</strong></p>
+                                        </div>
+                                   </div>
+                              </div>
+                              <div class="grid place-items-center bg-[#dce9ff] w-5 h-5 rounded-full self-end ml-4">
+                                   <svgo-panel-icon-offer-included class="w-[16px] h-[16px] fill-[#00378e]"/>
+                              </div>
+                         </div>
+                         <div class="flex flex-row justify-start items-center w-[calc(50%-2px)] shrink border border-[#e2e8ef] rounded-[4px] p-4 bg-white">sqd</div>
+
+                         <div class="flex flex-row justify-start items-center w-[calc(50%-2px)] shrink border border-[#e2e8ef] rounded-[4px] bg-[#f5f7f9] p-4">
+                              <svgo-panel-icon-chat-bull class="w-[20px] h-[20px] fill-[#647491]"/>
+                              <div class="inline-flex flex-grow max-w-full overflow-hidden ml-[16px]">
+                                   <div class="flex flex-row justify-start items-center max-w-full">
+                                        <div class="w-full">
+                                             <p class="mt-0 mb-0 font-normal text-[14px] leading-[18px] tracking-[-0.01em] text-left overflow-hidden text-ellipsis whitespace-nowrap text-[#080f1a] border-b border-dashed border-[#acb8cb] cursor-default">Conversations traitées - <strong>JUSQU’À 50</strong></p>
+                                        </div>
+                                   </div>
+                              </div>
+                              <div class="grid place-items-center bg-[#e2e8ef] w-5 h-5 rounded-full self-end ml-4">
+                                   <svgo-panel-icon-offer-excluded class="w-[16px] h-[16px] fill-[#acb8cb]"/>
+                              </div>
+                         </div>
+                         <div class="flex flex-row justify-start items-center w-[calc(50%-2px)] shrink border border-[#e2e8ef] rounded-[4px] bg-[#f5f7f9] p-4">sqd</div>
+
+                    </div>
+                    <button class="mt-5 rounded-[8px] text-[16px] leading-[20px] h-[38px] min-w-[80px] px-4 py-0
+  bg-[rgba(136,148,171,0)] border border-[#d1d9e0] text-[#333]">
+                         <span>Passer à une version antérieure</span>
+                    </button>
+               </div>
+          </div>
+     </div>
+</template>
+<script setup lang="ts">
+</script>
