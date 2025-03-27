868b351747b646470e2f41432c4c31a4d3f6864b
Jérémy Vandenostende <jrm@tiensdonc.com>
2025-03-27 09:43:16 +0100

    🧹 CHORE: Nettoyage du code

--- frontend/components/panel/upgrade/ModuleCard.vue
+++ frontend/components/panel/upgrade/ModuleCard.vue
@@ -67,20 +67,13 @@
                </div>
 
                <!-- Si multi-choix ET module coché, on affiche le menu déroulant -->
-               <div
-                    v-if="module.multipleChoice && checked"
-                    class="flex flex-col justify-start items-[normal] px-[24px] pb-[24px] pt-0"
-               >
+               <div v-if="module.multipleChoice && checked" class="flex flex-col justify-start items-[normal] px-[24px] pb-[24px] pt-0">
                     <div class="flex flex-row justify-start items-center">
-                         <p
-                              class="mt-0 mb-0 font-normal text-[14px] leading-[18px] tracking-[-0.01em] text-[#080f1a]"
-                         >
+                         <p class="mt-0 mb-0 font-normal text-[14px] leading-[18px] tracking-[-0.01em] text-[#080f1a]">
                               Choisissez une option :
                          </p>
                          <!-- info-bulle d'exemple -->
-                         <div
-                              class="shadow-[0px_1px_3px_0px_rgba(136,148,171,0.56)] bg-white rounded-[4px] w-[18px] h-[18px] ml-[8px] cursor-pointer flex items-center justify-center"
-                         >
+                         <div class="shadow-[0px_1px_3px_0px_rgba(136,148,171,0.56)] bg-white rounded-[4px] w-[18px] h-[18px] ml-[8px] cursor-pointer flex items-center justify-center">
                               <svgo-panel-icon-helper class="w-[14px] h-[14px] fill-[#647491]" />
                          </div>
                     </div>
@@ -88,10 +81,7 @@
 
                     <div class="flex flex-col justify-start items-[normal]">
                          <div class="mb-0 text-left relative w-full">
-                              <select
-                                   class="block w-full h-[34px] text-[14px] leading-[18px] border border-[#d3dbe5] rounded-[6px] px-[8px]"
-                                   @change="onChoiceChange($event)"
-                              >
+                              <select class="block w-full h-[34px] text-[14px] leading-[18px] border border-[#d3dbe5] rounded-[6px] px-[8px]" @change="onChoiceChange($event)">
                                    <option
                                         v-for="(choice, idx) in module.choices"
                                         :key="idx"
@@ -108,19 +98,12 @@
      </Tooltip>
 
      <!-- Si aucun tooltip n'est nécessaire (pas comingSoon, pas included) -->
-     <div
-          v-else
-          class="flex flex-col justify-start items-[normal] border border-[#e2e8ef] rounded-[12px]"
-          :class="[ index >= 1 ? 'mt-[16px]' : '' ]"
-     >
+     <div v-else class="flex flex-col justify-start items-[normal] border border-[#e2e8ef] rounded-[12px]" :class="[ index >= 1 ? 'mt-[16px]' : '' ]">
           <!-- Même contenu que ci-dessus, sans l’enveloppe Tooltip -->
           <div class="flex flex-col justify-start items-[normal] p-[20px]">
                <div class="flex flex-row justify-start items-center">
                     <div class="flex flex-col mr-auto">
-                         <label
-                              v-if="!checked"
-                              class="relative inline-block align-top rounded-[17px] border border-[#d3dbe5] bg-[#f5f7f9] cursor-pointer w-[36px] h-[22px]"
-                         >
+                         <label v-if="!checked" class="relative inline-block align-top rounded-[17px] border border-[#d3dbe5] bg-[#f5f7f9] cursor-pointer w-[36px] h-[22px]">
                               <input
                                    type="checkbox"
                                    role="switch"
@@ -129,14 +112,11 @@
                                    :disabled="false"
                                    @change="toggle(true)"
                               />
-                              <div
-                                   class="absolute left-[1px] top-[1px] bottom-[1px] w-[18px] rounded-full bg-white transition-[left,transform] duration-200 ease-in-out shadow-[0px_2px_8px_rgba(0,20,51,0.28)]"
+                              <div class="absolute left-[1px] top-[1px] bottom-[1px] w-[18px] rounded-full bg-white transition-[left,transform] duration-200 ease-in-out shadow-[0px_2px_8px_rgba(0,20,51,0.28)]"
                               ></div>
                          </label>
 
-                         <label
-                              v-else
-                              class="relative inline-block align-top rounded-[17px] border border-[#0566ff] bg-[#0566ff] cursor-pointer w-[36px] h-[22px] shadow-[inset_0px_0px_0px_11px_#0566ff]"
+                         <label v-else class="relative inline-block align-top rounded-[17px] border border-[#0566ff] bg-[#0566ff] cursor-pointer w-[36px] h-[22px] shadow-[inset_0px_0px_0px_11px_#0566ff]"
                          >
                               <input
                                    type="checkbox"
@@ -146,70 +126,44 @@
                                    :disabled="false"
                                    @change="toggle(false)"
                               />
-                              <div
-                                   class="absolute left-[calc(100%-1px)] top-[1px] bottom-[1px] w-[18px] rounded-full bg-white transition-[left,transform] duration-200 ease-in-out shadow-[0px_2px_8px_rgba(0,20,51,0.28)] translate-x-[-100%] translate-y-0"
+                              <div class="absolute left-[calc(100%-1px)] top-[1px] bottom-[1px] w-[18px] rounded-full bg-white transition-[left,transform] duration-200 ease-in-out shadow-[0px_2px_8px_rgba(0,20,51,0.28)] translate-x-[-100%] translate-y-0"
                               ></div>
                          </label>
                          <span class="block w-[12px] h-[12px]"></span>
                     </div>
 
                     <div class="flex flex-col self-end">
-                         <p
-                              v-if="module.multipleChoice && !checked"
-                              class="mt-0 mb-0 font-normal text-[11px] leading-[14px] tracking-[-0.01em] text-right text-[#647491]"
-                         >
-                              À partir de
-                         </p>
+                         <p v-if="module.multipleChoice && !checked" class="mt-0 mb-0 font-normal text-[11px] leading-[14px] tracking-[-0.01em] text-right text-[#647491]">À partir de</p>
                          <span class="block w-[2px] h-[2px]"></span>
                     </div>
                </div>
 
                <div class="flex flex-row justify-start items-center">
                     <div class="flex flex-col justify-start items-[normal] flex-grow">
-                         <h2
-                              class="mt-0 mb-0 font-medium text-[18px] leading-[24px] tracking-[-0.01em] text-[#080f1a]"
-                         >
+                         <h2 class="mt-0 mb-0 font-medium text-[18px] leading-[24px] tracking-[-0.01em] text-[#080f1a]">
                               {{ module.name }}
                          </h2>
-                         <p
-                              class="mt-[8px] mb-0 font-normal text-[14px] leading-[18px] tracking-[-0.01em] text-[#647491]"
-                         >
+                         <p class="mt-[8px] mb-0 font-normal text-[14px] leading-[18px] tracking-[-0.01em] text-[#647491]">
                               {{ module.description }}
                          </p>
                     </div>
 
                     <div class="flex flex-col justify-start items-[normal] whitespace-nowrap h-full ml-[12px]">
                          <div class="flex flex-row justify-start items-center">
-            <span>
-              <span
-                   class="h-auto relative text-[24px] leading-[31px] tracking-[-0.01em] text-[#080f1a] font-medium"
-              >
-                {{ displayedPrice }}<span
-                   class="text-[24px] leading-[31px] font-medium"
-              >€</span
-              >
-              </span>
-              <span
-                   class="text-[12px] leading-[16px] tracking-[-0.01em] text-[#080f1a] font-medium"
-              >{{ billingCycle === 'monthly' ? '/mois' : '/an' }}</span
-              >
-            </span>
+                              <span>
+                                   <span class="h-auto relative text-[24px] leading-[31px] tracking-[-0.01em] text-[#080f1a] font-medium">{{ displayedPrice }}<span class="text-[24px] leading-[31px] font-medium">€</span>
+                                   </span>
+                                   <span class="text-[12px] leading-[16px] tracking-[-0.01em] text-[#080f1a] font-medium">{{ billingCycle === 'monthly' ? '/mois' : '/an' }}</span>
+                              </span>
                          </div>
                     </div>
                </div>
           </div>
 
-          <div
-               v-if="module.multipleChoice && checked"
-               class="flex flex-col justify-start items-[normal] px-[24px] pb-[24px] pt-0"
-          >
+          <div v-if="module.multipleChoice && checked" class="flex flex-col justify-start items-[normal] px-[24px] pb-[24px] pt-0">
                <div class="flex flex-row justify-start items-center">
-                    <p class="mt-0 mb-0 font-normal text-[14px] leading-[18px] tracking-[-0.01em] text-[#080f1a]">
-                         Choisissez une option :
-                    </p>
-                    <div
-                         class="shadow-[0px_1px_3px_0px_rgba(136,148,171,0.56)] bg-white rounded-[4px] w-[18px] h-[18px] ml-[8px] cursor-pointer flex items-center justify-center"
-                    >
+                    <p class="mt-0 mb-0 font-normal text-[14px] leading-[18px] tracking-[-0.01em] text-[#080f1a]">Choisissez une option :</p>
+                    <div class="shadow-[0px_1px_3px_0px_rgba(136,148,171,0.56)] bg-white rounded-[4px] w-[18px] h-[18px] ml-[8px] cursor-pointer flex items-center justify-center">
                          <svgo-panel-icon-helper class="w-[14px] h-[14px] fill-[#647491]" />
                     </div>
                </div>
@@ -217,10 +171,7 @@
 
                <div class="flex flex-col">
                     <div class="mb-0 text-left relative w-full">
-                         <select
-                              class="block w-full h-[34px] text-[14px] leading-[18px] border border-[#d3dbe5] rounded-[6px] px-[8px]"
-                              @change="onChoiceChange($event)"
-                         >
+                         <select class="block w-full h-[34px] text-[14px] leading-[18px] border border-[#d3dbe5] rounded-[6px] px-[8px]" @change="onChoiceChange($event)">
                               <option
                                    v-for="(choice, idx) in module.choices"
                                    :key="idx"
