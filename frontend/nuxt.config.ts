3e62c88f6ec5c3f06c6c2d3ec02a132bfc1836fe
Jérémy Vandenostende <jrm@tiensdonc.com>
2025-04-14 09:00:12 +0200

    🌐 FEATURE: Prise en charge des meta, title et favicon

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -11,6 +11,20 @@ export default defineNuxtConfig({
        '@nuxtjs/i18n',
        '@nuxtjs/supabase'
      ],
+     app: {
+          head: {
+               title: 'AI Service Chatbot | Hellohumans',
+               meta: [
+                    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+                    { charset: 'utf-8' },
+                    { name: 'description', content: 'Hellohumans, is an AI customer service agent that instantly resolves up to 64% of customer queries. It understands customer intentions, responds in natural language and automates repetitive tasks.' },
+                    { name: 'theme-color', content: '#ffffff' }
+               ],
+               link: [
+                    { rel: 'icon', type: 'image/x-icon', href: '/favicon.svg' }
+               ]
+          }
+     },
      css: [
           '@/assets/css/fonts/mulish.css',
           '@/assets/css/fonts/euclidcircularb.css',
