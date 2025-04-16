468ff6a4f66db89aef23e142429e985ad4afe2f1
Benoit Chastel <131172923+benTDC@users.noreply.github.com>
2025-04-16 18:42:50 +0200

    🌐 FEAT: Ajout des t('clé') dans components/onboarding pour l’internationalisation

--- frontend/components/panel/onboarding/step/Part1.vue
+++ frontend/components/panel/onboarding/step/Part1.vue
@@ -1,21 +1,31 @@
 <template>
      <div class="flex flex-col gap-[32px] sm:gap-[48px]">
 
-          <PanelOnboardingSection title="Sélectionnez ou créez un compte client" description="Votre projet sera rattaché à ce compte client">
-               <PanelOnboardingSelectClient v-model="onboardingStore.answers.selectedClientId" :clients="panelStore.clients" placeholder="Sélectionnez un compte client" placeholderCreate="Création d’un nouveau compte client"/>
+          <PanelOnboardingSection
+               :title="t('panel.components.onboarding.step.part1.selectClient.title')"
+               :description="t('panel.components.onboarding.step.part1.selectClient.description')">
+               <PanelOnboardingSelectClient
+                    v-model="onboardingStore.answers.selectedClientId" :clients="panelStore.clients"
+                    :placeholder="t('panel.components.onboarding.step.part1.selectClient.placeholder')"
+                    :placeholderCreate="t('panel.components.onboarding.step.part1.selectClient.placeholderCreate')"/>
           </PanelOnboardingSection>
 
 
           <!-- SECTION 1 : URL du site -->
-          <PanelOnboardingSection title="Quelle est l'adresse URL de votre site web" description="Vos réponses nous aideront à personnaliser votre expérience">
+          <PanelOnboardingSection
+               :title="t('panel.components.onboarding.step.part1.siteUrl.title')"
+               :description="t('panel.components.onboarding.step.part1.siteUrl.description')">
                <label class="block font-normal max-w-full flex flex-col relative text-[16px] leading-[20px] tracking-[-0.01em] w-full text-left text-[#647491]">
-                    <input type="text" v-model="onboardingStore.answers.webSite" placeholder="https://votre-site.com" class="block w-full h-[51px] text-[#080f1a] border-2 border-[#d3dbe5] rounded-[8px] px-[14px] py-[16px] outline-none focus:border-[#3886ff]"/>
+                    <input type="text"
+                           v-model="onboardingStore.answers.webSite" :placeholder="t('panel.components.onboarding.step.part1.siteUrl.placeholder')"
+                           class="block w-full h-[51px] text-[#080f1a] border-2 border-[#d3dbe5] rounded-[8px] px-[14px] py-[16px] outline-none focus:border-[#3886ff]"/>
                </label>
           </PanelOnboardingSection>
      </div>
 </template>
 
 <script setup lang="ts">
+const { t } = useI18n()
 const panelStore = usePanelStore()
 const onboardingStore = useOnboardingStore()
 
