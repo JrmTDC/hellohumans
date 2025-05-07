a7f989ee20c85c79658d775d596afd54f67fa242
Jérémy Vandenostende <jrm@tiensdonc.com>
2025-05-08 01:18:30 +0200

    🐛BUGFIX: Correction du positionnement des menus Help et Utilisation

--- frontend/components/panel/layout/LayoutHeaderPage.vue
+++ frontend/components/panel/layout/LayoutHeaderPage.vue
@@ -5,7 +5,7 @@
                     {{ title }}
                     <div v-if="isBilled" :class="isPaid ? 'bg-[rgb(190,255,203)]' : 'bg-[rgb(255,219,186)]'" class="p-[3px_5px] rounded-[4px] block ml-[8px]" > <div class="flex flex-row justify-start items-center"> <svgo-panel-icon-premium class="w-[14px] h-[14px] block fill-[rgb(8,15,26)] transition-none mr-0" /> <p class="mt-0 mb-0 ml-[2px] font-normal text-[12px] leading-[16px] tracking-[-0.01em]"> {{ isPaid ? t('panel.components.layout.herderPage.UsageMenu.paid') : t('panel.components.layout.herderPage.UsageMenu.billing') }} </p> </div> </div>
                </h1>
-               <div class="flex flex-row justify-start items-center ml-[16px]" ref="helpMenuRef">
+               <div class="relative flex flex-row justify-start items-center ml-[16px]" ref="helpMenuRef">
                     <div @click="toggleHelpMenu" class="inline-flex relative align-top">
                          <button class="bg-transparent border-0 inline-flex shadow-none outline-none px-[4px] py-0 items-center justify-center cursor-pointer rounded-[4px] text-current w-[30px] h-[30px] hover:bg-[#dce9ff] group">
                               <svgo-panel-icon-help class="w-[22px] h-[22px] fill-[#8796af] group-hover:fill-[#0566ff]" />
@@ -13,8 +13,8 @@
                     </div>
 
                     <div v-if="isHelpMenuOpen" class="usage-dropdown">
-                         <div class="absolute right-[32px] top-[53px] z-[2]"><svgo-panel-icon-triangle-up class="w-[17px] h-[7px] fill-[#ffff]" /></div>
-                         <div class="absolute top-[60px] right-[25px] box-content w-fit bg-white p-[8px] shadow-[0_8px_20px_rgba(0,20,51,0.24)] rounded-[8px] overflow-auto max-h-[calc(-150px+100vh)] z-[1]">
+                         <div class="absolute right-[8px] top-[42px] z-[2]"><svgo-panel-icon-triangle-up class="w-[17px] h-[7px] fill-[#ffff]" /></div>
+                         <div class="absolute top-[49px] right-[0] box-content w-fit bg-white p-[8px] shadow-[0_8px_20px_rgba(0,20,51,0.24)] rounded-[8px] overflow-auto max-h-[calc(-150px+100vh)] z-[1]">
 
                               <div class="w-full flex items-center text-[#080f1a] bg-transparent border-none rounded-[4px] min-h-[36px] px-[8px] py-[6px] cursor-pointer outline-none hover:bg-[#dce9ff] hover:text-[#001433] group">
                                    <span class="whitespace-nowrap overflow-hidden text-ellipsis flex-1 text-left text-[14px]">
@@ -34,7 +34,7 @@
                          </div>
                     </div>
                </div>
-               <div class="flex flex-row justify-start items-center ml-[16px]" ref="usageMenuRef">
+               <div class="relative flex flex-row justify-start items-center ml-[16px]" ref="usageMenuRef">
 
                     <div @click="toggleUsageMenu" class="flex ml-[8px] flex-row justify-start items-center p-[6px] rounded-[6px] cursor-pointer text-[rgb(58,35,0)] bg-transparent hover:bg-[rgb(220,233,255)] group">
                          <svgo-panel-icon-usage-menu class="w-[20px] h-[20px] fill-[#8796af] transition-transform group-hover:fill-[rgb(5,102,255)]" :class="{ 'rotate-180': isUsageMenuOpen }" />
@@ -44,8 +44,8 @@
                          <svgo-panel-icon-triangle-caret-down class="w-[24px] h-[24px fill-[#8796af] transition-transform group-hover:fill-[rgb(135,150,175)]" :class="{ 'rotate-180': isUsageMenuOpen }" />
                     </div>
                     <div v-if="isUsageMenuOpen" class="usage-dropdown">
-                         <div class="absolute right-[32px] top-[53px] z-[2]"><svgo-panel-icon-triangle-up class="w-[17px] h-[7px] fill-[#ffff]" /></div>
-                         <div class="absolute top-[60px] right-[25px] box-content w-[412px] bg-white p-[24px] shadow-[0_8px_20px_rgba(0,20,51,0.24)] rounded-[8px] overflow-auto max-h-[calc(-150px+100vh)] z-[1]">
+                         <div class="absolute right-[8px] top-[42px] z-[2]"><svgo-panel-icon-triangle-up class="w-[17px] h-[7px] fill-[#ffff]" /></div>
+                         <div class="absolute top-[49px] right-[0] box-content w-[412px] bg-white p-[24px] shadow-[0_8px_20px_rgba(0,20,51,0.24)] rounded-[8px] overflow-auto max-h-[calc(-150px+100vh)] z-[1]">
                               <PanelCommonHeaderUsageItem
                                    v-for="(item, index) in usages"
                                    :key="index"
